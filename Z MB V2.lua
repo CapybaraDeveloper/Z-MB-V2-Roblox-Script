278ad1fc419472ab56787b41bc322b2e68b259c1
CapybaraDev <149486051+CapybaraDeveloper@users.noreply.github.com>
2024-11-03 09:25:16 +0500

    Add files via upload

---
+++ Z MB V2.lua
@@ -0,0 +1,63 @@
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+
+local Window = OrionLib:MakeWindow({Name = "Z MB Version 2.0", HidePremium = false, SaveConfig = true, ConfigFolder = "Z MB 2.0"})
+
+OrionLib:MakeNotification({
+	Name = "Succesful injected!",
+	Content = "You succesful inject script!",
+	Image = "rbxassetid://4483345998",
+	Time = 5
+})
+
+local MainTab = Window:MakeTab({
+	Name = "Main",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+local Section = MainTab:AddSection({
+	Name = "Main Tab"
+})
+
+MainTab:AddButton({
+	Name = "Namelles Admin",
+	Callback = function()
+		loadstring(game:HttpGet("https://raw.githubusercontent.com/ltseverydayyou/Nameless-Admin/main/Source.lua"))();
+	end    
+})
+
+MainTab:AddButton({
+	Name = "Evade script",
+	Callback = function()
+		loadstring(game:HttpGet('https://whimper.xyz/kitty'))();
+	end    
+})
+
+MainTab:AddButton({
+	Name = "Blox Fruit(Main mobail script)",
+	Callback = function()
+		loadstring(game:HttpGet("https://raw.githubusercontent.com/REDzHUB/BloxFruits/main/redz9999"))()
+	end    
+})
+
+
+local SecondTab = Window:MakeTab({
+	Name = "Script Tools",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+local Section = SecondTab:AddSection({
+	Name = "Script Tools"
+})
+
+SecondTab:AddButton({
+	Name = "Destroy Hub",
+	Callback = function()
+		OrionLib:Destroy()
+	end    
+})
+
+SecondTab:AddParagraph("1","Main scripts")
+
+SecondTab:AddLabel("Discord '.gg/PCAAmXE2DY' ")
